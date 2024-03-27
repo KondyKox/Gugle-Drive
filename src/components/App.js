7be04697baf331ba55cf5fc431e7c2af26c611b6
Konrad <kondykox@gmail.com>
2024-03-27 20:44:38 +0100

    sign up component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,19 @@
+import React from "react";
+import Signup from "./Signup";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+
 function App() {
-  return "Hello World";
+  return (
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Signup />;
+      </div>
+    </Container>
+  );
 }
 
 export default App;
