c8207eff9c20c308926c004d597b4c62f15d2d01
Konrad <kondykox@gmail.com>
2024-03-28 15:43:05 +0100

    update profile

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ import Dashboard from "./Dashboard";
 import Login from "./Login";
 import PrivateRoute from "./PrivateRoute";
 import ForgotPassword from "./ForgotPassword";
+import UpdateProfile from "./UpdateProfile";
 
 function App() {
   return (
@@ -20,6 +21,11 @@ function App() {
           <AuthProvider>
             <Switch>
               <PrivateRoute exact path="/" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/update-profile"
+                component={UpdateProfile}
+              />
               <Route path="/signup" component={Signup} />
               <Route path="/login" component={Login} />
               <Route path="/forgot-password" component={ForgotPassword} />
--- src/components/UpdateProfile.js
+++ src/components/UpdateProfile.js
@@ -3,30 +3,48 @@ import { Card, Form, Button, Alert } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
 import { Link, useHistory } from "react-router-dom";
 
-const Signup = () => {
+const UpdateProfile = () => {
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfimRef = useRef();
-  const { signup } = useAuth();
+  const { currentUser, updatePassword, updateEmail } = useAuth();
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   const history = useHistory();
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
     if (passwordRef.current.value !== passwordConfimRef.current.value)
       return setError("Passwords do not match");
 
+    const promises = [];
+    setLoading(true);
+    setError("");
+
+    if (emailRef.current.value !== currentUser.email)
+      promises.push(updateEmail(emailRef.current.value));
+    if (passwordRef.current.value)
+      promises.push(updatePassword(passwordRef.current.value));
+
+    Promise.all(promises)
+      .then(() => {
+        history.push("/");
+      })
+      .catch(() => {
+        setError("Failed to update account.");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+
     try {
       setError("");
       setLoading(true);
-      await signup(emailRef.current.value, passwordRef.current.value);
       history.push("/");
     } catch {
       setError("Failed to create an account");
     }
-
     setLoading(false);
   };
 
@@ -34,35 +52,48 @@ const Signup = () => {
     <>
       <Card>
         <Card.Body>
-          <h2 className="text-center mb-4">Sign Up</h2>
+          <h2 className="text-center mb-4">Update Profile</h2>
           {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group id="email" className="mt-2">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
+              <Form.Control
+                type="email"
+                ref={emailRef}
+                required
+                defaultValue={currentUser.email}
+              />
             </Form.Group>
 
             <Form.Group id="password" className="mt-2">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" ref={passwordRef} required />
+              <Form.Control
+                type="password"
+                ref={passwordRef}
+                placeholder="Leave blank to keep the same"
+              />
             </Form.Group>
 
             <Form.Group id="password-confirm" className="mt-2">
               <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type="password" ref={passwordConfimRef} required />
+              <Form.Control
+                type="password"
+                ref={passwordConfimRef}
+                placeholder="Leave blank to keep the same"
+              />
             </Form.Group>
 
             <Button disabled={loading} className="w-100 mt-3" type="submit">
-              Sign Up
+              Update
             </Button>
           </Form>
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2">
-        Already have an account? <Link to="/login">Log in</Link>
+        <Link to="/">Cancel</Link>
       </div>
     </>
   );
 };
 
-export default Signup;
+export default UpdateProfile;
